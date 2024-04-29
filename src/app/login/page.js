2ba29cf323644213ad7479bc730f5d0579641af0
Omar Aglan <Omar.aglan91@gmail.com>
2024-04-29 13:52:01 +0300

    Update The Login (Not Working For Now)

--- src/app/login/page.js
+++ src/app/login/page.js
@@ -1,3 +1,18 @@
-export default function Login() {
-	return <div>Hi World</div>;
+import { getProviders } from 'next-auth/react';
+export default function Login({providers}) {
+	return (
+		<div>
+			{Object.values(providers).map(provider => (
+				<div>
+					<button>SignIn With {provider.name}</button>
+				</div>
+			))}
+		</div>
+	);
+}
+export async function getServerSideProps(context) {
+	const providers = await getProviders;
+	return {
+		props: { providers } // will be passed to the page component as props
+	};
 }
