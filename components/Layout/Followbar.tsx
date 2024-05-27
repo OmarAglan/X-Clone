f41da112bc92b91e98bf2fc5586683c02c21b899
Omar Aglan <Omar.aglan91@gmail.com>
2024-05-27 13:58:53 +0300

    Follow Bar

--- components/Layout/Followbar.tsx
+++ components/Layout/Followbar.tsx
@@ -1,7 +1,12 @@
 const FollowBar = () => {
     return (
-        <div>
-
+        <div className="px-9 py-3 hidden lg:block">
+            <div className="bg-neutral-900 rounded-xl p-5">
+                <h2 className="text-white text-xl font-bold ">You Might Know</h2>
+                <div className="flex flex-col gap-6 mt-4">
+                    {/*User List*/}
+                </div>
+            </div>
         </div>
     )}
 
